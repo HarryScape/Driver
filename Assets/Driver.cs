a721024a0e3ba47bc28bca4ab96f32a1870c5291
Harry Newman <Harry@live.fi>
2021-11-28 10:17:49 +0000

    Finished ish Game
    Noice

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
+[SerializeField] float steerSpeed = 205f;
+[SerializeField] float moveSpeed = 15f;
+//[SerializeField] float slowSpeed = 20f;
+[SerializeField] float fastSpeed = 40f;
+
     void Start()
     {
         //transform.Rotate(0, 0, 45);  // x, y, z
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, 0.1f);
-        transform.Translate(0, 0.01f, 0);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
+    
+    void OnTriggerEnter2D(Collider2D other) {
+            if (other.tag == "Boost") {
+                moveSpeed = fastSpeed;
+            }
+        }
 }
